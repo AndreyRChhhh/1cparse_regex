40084d79741cd16606a290fc05b775ea3a078ba9
AndreyRChhhh <andrey.rysanov@gmail.com>
2018-09-24 11:52:47 +0300

    1

--- main.py
+++ main.py
@@ -34,6 +34,8 @@ class Application:
         self.builder.get_object('Entry_2').insert(0,root.filename) #.insert(0,variable)
         in_file = root.filename
 
+
+
     def start_pr(self):
         my_file = self.builder.get_object('Entry_2').get()
         with open(my_file, 'r', encoding='utf-8') as read_file:
